f6eb9d708479ae4c1e34fe62ff6e579a70c22c63
Lucas Fagundes <163209635+zarkye@users.noreply.github.com>
2024-06-18 19:45:09 -0300

    Update funcoes.py

--- 02/funcoes.py
+++ 02/funcoes.py
@@ -3,7 +3,7 @@ def dobro_das_pares_triplo_das_impares_posicoes(lista) -> list:
     for i, n in enumerate(lista):
         if i % 2 == 0:
             resultado.append(n * 2)
-        elif i % 3 == 0:
+        else:
             resultado.append(n * 3)
 
     return resultado
