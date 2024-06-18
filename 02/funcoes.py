4c5f8eeff5f395764bfef2a0a2ccec1f275a2e8b
Lucas Fagundes <lucasfagundesice@gmail.com>
2024-06-18 16:12:38 -0300

    terminei a 2

---
+++ 02/funcoes.py
@@ -0,0 +1,9 @@
+def dobro_das_pares_triplo_das_impares_posicoes(lista) -> list:
+    resultado = []
+    for i, n in enumerate(lista):
+        if i % 2 == 0:
+            resultado.append(n * 2)
+        elif i % 3 == 0:
+            resultado.append(n * 3)
+
+    return resultado
---
+++ 02/main.py
@@ -0,0 +1,4 @@
+from funcoes import *
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+resultado = dobro_das_pares_triplo_das_impares_posicoes(lista)
+print(resultado)
